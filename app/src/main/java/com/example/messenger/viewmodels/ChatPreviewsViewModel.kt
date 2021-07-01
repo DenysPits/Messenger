e5fc7f397eed3854ddaf928c961701f0c9b0f975
Denys Pits <lykoshko4@gmail.com>
2021-07-01 12:21:36 +0300

    Initial commit. Add themes, registration UI

---
+++ app/src/main/java/com/example/messenger/viewmodels/ChatPreviewsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.messenger.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class ChatPreviewsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/messenger/viewmodels/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.messenger.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
