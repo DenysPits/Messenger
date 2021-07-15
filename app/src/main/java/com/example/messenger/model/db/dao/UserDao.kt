ee324e40eb4ca7f36131771b4d5f9931018c5b0e
Denys Pits <lykoshko4@gmail.com>
2021-07-15 13:25:03 +0300

    Complete chat ui, refactor chat previews

--- app/src/main/java/com/example/messenger/model/db/dao/UserDao.kt
+++ app/src/main/java/com/example/messenger/model/db/dao/UserDao.kt
@@ -17,13 +17,13 @@ interface UserDao {
 
     @Query("""
         SELECT users.id AS user_id, name, avatar, text AS message, time FROM users 
-        LEFT JOIN messages ON users.id=messages.companion_id 
+        LEFT JOIN messages ON users.id=messages.companion_id
         JOIN (SELECT companion_id, MAX(time) AS MaxTime FROM messages GROUP BY companion_id) messages_group 
-        ON CASE 
-        WHEN messages.text IS NULL THEN 1
-        ELSE messages.companion_id = messages_group.companion_id AND messages.time = messages_group.MaxTime
-        END 
-        WHERE is_my_user = 0"""
+        ON messages.companion_id = messages_group.companion_id AND messages.time = messages_group.MaxTime
+        WHERE is_my_user = 0
+        UNION SELECT users.id AS user_id, name, avatar, text AS message, time FROM users
+        LEFT JOIN messages ON users.id=messages.companion_id
+        WHERE is_my_user = 0 AND message IS NULL"""
     )
     fun getChatPreviews(): Flow<List<ChatPreview>>
 }
